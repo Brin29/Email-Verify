1eb1d54199362df5fafeabaa217e5688c8593548
Brin29 <breinerstiveparracortes50@gmail.com>
2024-11-27 17:06:37 -0500

    first commit

---
+++ src/main/java/com/brien/email_veify/EmailVeifyApplication.java
@@ -0,0 +1,13 @@
+package com.brien.email_veify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVeifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailVeifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brien/email_veify/EmailVeifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brien.email_veify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailVeifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
